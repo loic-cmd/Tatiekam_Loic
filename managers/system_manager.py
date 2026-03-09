f2c3a9dbe391412243e966d3ed1e6dd02103d7df
loic-cmd <loic@csss.es>
2026-03-09 14:08:19 +0100

    Add files via upload

--- managers/system_manager.py
+++ managers/system_manager.py
@@ -68,39 +68,39 @@ class SystemManager:
 
             elif choice == "3":
                 user = self.login()
-            if not user:
-                continue
+                if not user:
+                    continue
 
-            if user.get_role() == "Student":
-                while True:
-                    print("\n Student Menu")
-                    print("1. View courses")
-                    print("2. Logout")
+                if user.get_role() == "Student":
+                    while True:
+                        print("\n Student Menu")
+                        print("1. View courses")
+                        print("2. Logout")
 
-                    student_choice = input("Select an Option: ")
-                    if student_choice == "1":
-                        self.list_courses()
-                    elif student_choice == "2":
-                        break
-                    else:
-                        print("Invalid Option")
-            elif user.get_role() == "Admin":
-                while True:
-                    print("\n Admin Menu")
-                    print("1. Add Course")
-                    print("2. View Courses")
-                    print("3. Logout")
+                        student_choice = input("Select an Option: ")
+                        if student_choice == "1":
+                            self.list_courses()
+                        elif student_choice == "2":
+                            break
+                        else:
+                            print("Invalid Option")
+                elif user.get_role() == "Admin":
+                    while True:
+                        print("\n Admin Menu")
+                        print("1. Add Course")
+                        print("2. View Courses")
+                        print("3. Logout")
 
-                    admin_choice = input("Choose: ")
+                        admin_choice = input("Choose: ")
 
-                    if admin_choice == "1":
-                        self.add_course()
-                    elif admin_choice == "2":
-                        self.list_courses()
-                    elif admin_choice == "3":
-                        break
-                    else:
-                        print("Invalid Option")
+                        if admin_choice == "1":
+                            self.add_course()
+                        elif admin_choice == "2":
+                            self.list_courses()
+                        elif admin_choice == "3":
+                            break
+                        else:
+                            print("Invalid Option")
             elif choice == "4":
                 print("Exiting program...")
                 break
